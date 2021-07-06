084976c24962b17edd86016a66bd098313db2fe7
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-05 20:21:46 -0700

    fetch city

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
-console.log("t")
+const APIkey = "54f5d77c61f7b7d7da7d2c41a2956900";
+const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+
+$(document).ready(function () {
+
+    const searchCity = (e) => {
+        e.preventDefault();
+        let currentCity = $("#enter-city").val();
+        console.log(currentCity)
+        $("#enter-city").val("");
+        fetchCity(currentCity)
+    }
+
+    const fetchCity = (city) => {
+        let weatherURL = `${url + city}&appid=${APIkey}`
+        fetch(weatherURL)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+    // pressing search icon or on enter.
+    $(".fa-search").on("click", searchCity)
+    $("#search").submit(searchCity)
+
+
+});
