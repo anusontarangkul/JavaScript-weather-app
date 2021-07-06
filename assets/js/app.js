edb440d9573ce9b97e36080ba9f814c119f1757e
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-06 00:18:42 -0700

    save and load local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,47 @@
 const APIkey = "54f5d77c61f7b7d7da7d2c41a2956900";
 const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+let citiesArray = [];
 
 
 $(document).ready(function () {
+
 
     const searchCity = (e) => {
         e.preventDefault();
         let currentCity = $("#enter-city").val();
         console.log(currentCity)
         $("#enter-city").val("");
-        fetchCity(currentCity)
+        recentSearch(currentCity);
+        // fetchCity(currentCity)
     }
 
-    const fetchCity = (city) => {
-        let weatherURL = `${url + city}&appid=${APIkey}`
-        fetch(weatherURL)
-            .then(res => res.json())
-            .then(data => console.log(data))
+    // const fetchCity = (city) => {
+    //     let weatherURL = `${url +city}&appid=${APIkey}`
+    //     fetch(weatherURL)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }
+
+    const recentSearch = (search) => {
+        let newButton = $('<button/>',
+            {
+                text: search,
+                "class": "recent-btn"
+            })
+        $("#history-container").append(newButton);
+        citiesArray.push(search)
+        localStorage.setItem("savedCities", citiesArray)
     }
 
+    const loadSearch = () => {
+        let citiesToLoad = localStorage.getItem("savedCities");
+        let citiesToLoadArray = citiesToLoad.split(",")
+        console.log(citiesToLoadArray)
+
+        console.log(typeof citiesToLoadArray)
+        citiesToLoadArray.forEach(city => recentSearch(city))
+    }
+    loadSearch();
     // pressing search icon or on enter.
     $(".fa-search").on("click", searchCity)
     $("#search").submit(searchCity)
